82fc94a052f470ff299e8131bbe2437b508b5bd7
Johan Serebrink <johan.serebrink@outlook.com>
2014-11-04 14:05:57 +0100

    Epic code added, must merge!

--- SMART/Program.cs
+++ SMART/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,7 @@ namespace SMART
 	{
 		static void Main(string[] args)
 		{
-
+			Debug.WriteLine("Jag är bäst, du är en häst!");
 		}
 	}
 }
